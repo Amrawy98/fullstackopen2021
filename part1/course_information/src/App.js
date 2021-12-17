c62c11547c1fe2c5208d2a10ebbfd311678ce690
Amrawy98 <ahmed.m.amrawy@gmail.com>
2021-12-17 18:40:55 +0200

    add exercise 1.1

---
+++ part1/course_information/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  const parts = props.parts.map((part) => (
+    <p>
+      {part.name} {part.count}
+    </p>
+  ));
+  return <div>{parts}</div>;
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts.reduce((a, b) => a + b.count, 0)}</p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const parts = [
+    { name: part1, count: exercises1 },
+    { name: part2, count: exercises2 },
+    { name: part3, count: exercises3 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
