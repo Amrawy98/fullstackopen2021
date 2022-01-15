10d96defeeb75b7e4d85e9c8aa58f49c525ce303
Amrawy <ahmed.m.amrawy@gmail.com>
2022-01-15 18:20:16 +0200

    add the solution to exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 const Person = ({ person }) => (
   <div>
     {person.name} {person.number}
@@ -38,16 +38,17 @@ const PersonForm = ({
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   let id = 5;
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchPhrase, setSearchPhrase] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data));
+  }, []);
 
   const getSearchResult = () =>
     persons.filter((person) => {
